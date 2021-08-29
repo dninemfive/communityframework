1a5703b922aaf4dc386228f77474a26ed9d8e6d7
Turnovus <dinolover075@gmail.com>
2021-08-29 10:28:47 -0400

    Ability to play sounds upon equipping clothing
    Added ThingCOmp_OnWearSound, which can be applied to apparel and can play a sound when the apparel is worn or removed. Source code still needs commenting.

---
+++ Source/communityframework/communityframework/Comps/ThingComps/OnWearSound/ThingCompProperties_OnWearSound.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace CF
+{
+    class ThingCompProperties_OnWearSound : CompProperties
+    {
+        public SoundDef sound;
+
+        public ThingCompProperties_OnWearSound() =>
+            compClass = typeof(ThingComp_OnWearSound);
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            List<string> errors =
+                new List<string>(base.ConfigErrors(parentDef));
+            if (!parentDef.IsApparel)
+            {
+                errors.Add("ThingCompProperties_OnWearSound applied to " +
+                    "ThingDef " + parentDef.label + ", which is not an " +
+                    "apparel item.");
+            }
+            return errors.AsEnumerable();
+        }
+    }
+}
---
+++ Source/communityframework/communityframework/Comps/ThingComps/OnWearSound/ThingComp_OnWearSound.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.Sound;
+using RimWorld;
+
+namespace CF
+{
+    class ThingComp_OnWearSound : ThingComp
+    {
+        ThingCompProperties_OnWearSound Props =>
+            (ThingCompProperties_OnWearSound)props;
+        public override void Notify_Equipped(Pawn pawn)
+        {
+            Props.sound.PlayOneShot(
+                new TargetInfo(pawn.Position, pawn.Map, false));
+            base.Notify_Equipped(pawn);
+        }
+    }
+}
