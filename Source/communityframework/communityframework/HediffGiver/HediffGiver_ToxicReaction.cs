a3d8f249b8983e6e91b75d92bc7fa45eebd44312
Turnovus <dinolover075@gmail.com>
2021-10-01 16:39:14 -0400

    Added HediffGiver_ToxicReaction

---
+++ Source/communityframework/communityframework/HediffGiver/HediffGiver_ToxicReaction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace CF
+{
+    class HediffGiver_ToxicReaction : HediffGiver
+    {
+        public HediffDef hediffDef;
+
+        public override bool OnHediffAdded(Pawn pawn, Hediff hediff)
+        {
+            if (hediff.def == HediffDefOf.ToxicBuildup)
+            {
+                HealthUtility.AdjustSeverity(pawn, hediffDef, hediff.Severity);
+                pawn.health.RemoveHediff(hediff);
+            }
+            return base.OnHediffAdded(pawn, hediff);
+        }
+    }
+}
