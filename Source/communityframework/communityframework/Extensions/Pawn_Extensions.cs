de9d24fa1123779f470e9859b1d2d28905ec9298
Turnovus <dinolover075@gmail.com>
2021-10-06 04:48:39 -0400

    Pawn_Extensions documentation
    XML documentation for Pawn_Extensions, which was missed during last commit. Also, rebuilt solution.

--- Source/communityframework/communityframework/Extensions/Pawn_Extensions.cs
+++ Source/communityframework/communityframework/Extensions/Pawn_Extensions.cs
@@ -8,13 +8,30 @@ using RimWorld;
 
 namespace CF
 {
+    /// <summary>
+    /// Additional extensions applied to the <c>Pawn</c> class.
+    /// </summary>
     public static class Pawn_Extensions
     {
+        /// <summary>
+        /// Used to determine if a <c>Pawn</c> has one of multiple races.
+        /// </summary>
+        /// <param name="pawn">The <c>Pawn</c> to check.</param>
+        /// <param name="races">A list of races to check for.</param>
+        /// <returns><c>true</c> if <c>pawn</c>'s race is contained in
+        /// <c>races</c>.</returns>
         public static bool IsRace(this Pawn pawn, List<ThingDef> races)
         {
             return races.Contains(pawn.def);
         }
 
+        /// <summary>
+        /// Used to determine if a <c>Pawn</c> is of a specific race.
+        /// </summary>
+        /// <param name="pawn">The <c>Pawn</c> to check.</param>
+        /// <param name="race">The race to check for.</param>
+        /// <returns><c>true</c> if <c>pawn</c>'s race is equal to <c>race</c>.
+        /// </returns>
         public static bool IsRace(this Pawn pawn, ThingDef race)
         {
             return pawn.IsRace(new List<ThingDef>() { race });
